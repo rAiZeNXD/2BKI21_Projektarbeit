74b8882f5c25663bcdcf02a79bf0eb2777cf7f31
rAiZeNXD <kamil.pruszek1@gmail.com>
2021-03-04 18:26:56 +0100

    Bug: Man kann nicht letztes Betriebsmodus wählen

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,12 @@ char dataSent[30];
 // Index für die while-Schleife in der checkSerial() Funktion
 int i = 0;
 
-// Enum für die Steuerung von Ventilen (Zustände) 
-enum states { open, closed };
-states controlsV1 ,controlsV2, controlsV3, controlsV4;
+// Enum für Betriebmodus
+enum Betriebmodus { start, debug, manual, automatic };
+Betriebmodus modus;
+
+enum Zustaende { open, closed };
+Zustaende stateV1, stateV2, stateV3, stateV4;
 
 // 
 String dataRecieved;
@@ -29,85 +32,110 @@ String dataRecieved;
 long timeNow, timePrev;
 
 // Funktionsprototypen (Notwendig in PlatformIO)
-void checkSerial(void);
-void automatSteuerung(void);
+void steuerung(void);
 
-void setup() {
+void setup() 
+{
   pinMode(V1, INPUT);
   pinMode(V2, INPUT);
   pinMode(V3, INPUT);
   pinMode(V4, INPUT);
+  delay(750);
   Serial.begin(115200);
+  modus = start;
 }
 
-void loop() {
-    checkSerial();
-    automatSteuerung();
-    delay(700);
+void loop() 
+{
+    steuerung();
+    delay(500);
 }
 
 /* Diese Funktion überprüft ob etwas über die Serielle Schnitstelle gesendet wurde
 #  Wenn ja, dann speichert es die einzele Bits in CharArray und dann umwandelt CharArray in String um */ 
-void checkSerial()
+void steuerung()
 {
   while (Serial.available() > 0 && Serial.available() < 30)
   {
     dataSent[i++] = Serial.read();
   }
   dataRecieved = dataSent;
-  Serial.println("DEBUG (dataRecoeved): " + dataRecieved);    //Gibt ein String aus was über die Serielle Schnitstelle empafngen wurde (Debug)
-  i = 0; 
+  //Serial.println("DEBUG (dataRecoeved): " + dataRecieved);    Gibt ein String aus was über die Serielle Schnitstelle empafngen wurde (Debug)
 
-  if (dataRecieved == "1 open") { controlsV1 = open; }
-  else if (dataRecieved == "1 close") { controlsV1 = closed; }
-  else if (dataRecieved == "2 open") { controlsV2 = open; }
-  else if (dataRecieved == "2 close") { controlsV2 = closed; }
-  else if (dataRecieved == "3 open") { controlsV3 = open; }
-  else if (dataRecieved == "3 close") { controlsV3 = closed; }
-  else if (dataRecieved == "4 open") { controlsV4 = open; }
-  else if (dataRecieved == "4 close") { controlsV4 = closed; }
-}
+  if (dataRecieved == "debug") { modus = debug; }
+  else if (dataRecieved == "manual") { modus = manual; }
+  else if (dataRecieved == "automatic") { modus = automatic; }
 
-void automatSteuerung()
-{
-  if (controlsV1 == open) 
-  { 
-    digitalWrite(V1, HIGH); 
-    Serial.println("DEBUG (Zustand): 1 open"); 
-  }
-  else if (controlsV1 == closed) 
-  { 
-    digitalWrite(V1, LOW);
-    Serial.println("DEBUG (Zustand): 1 closed");  
-  }
-  else if (controlsV2 == open) 
-  { 
-    digitalWrite(V2, HIGH);
-    Serial.println("DEBUG (Zustand): 2 open");
-  }
-  else if (controlsV2 == closed) 
-  { 
-    digitalWrite(V2, LOW);
-    Serial.println("DEBUG (Zustand): 2 closed");
-  }
-  else if (controlsV3 == open) 
-  { 
-    digitalWrite(V3, HIGH);
-    Serial.println("DEBUG (Zustand): 3 open");
-  }
-  else if (controlsV3 == closed) 
-  { 
-    digitalWrite(V3, LOW);
-    Serial.println("DEBUG (Zustand): 3 closed");   
-  } 
-  else if (controlsV4 == open) 
-  { 
-    digitalWrite(V4, HIGH);
-    Serial.println("DEBUG (Zustand): 4 open");
-  }
-  else if (controlsV4 == closed)
-  { 
-    digitalWrite(V4, LOW);
-    Serial.println("DEBUG (Zustand): 4 closed");
+  // Befehle für Debug Steuerung
+  else if (dataRecieved == "1 open") { stateV1 = open; }
+  else if (dataRecieved == "1 close") { stateV1 = closed; }
+  else if (dataRecieved == "2 open") { stateV2 = open; }
+  else if (dataRecieved == "2 close") { stateV2 = closed; }
+  else if (dataRecieved == "3 open") { stateV3 = open; }
+  else if (dataRecieved == "3 close") { stateV3 = closed; }
+  else if (dataRecieved == "4 open") { stateV4 = open; }
+  else if (dataRecieved == "4 close") { stateV1 = closed; }
+  
+  i = 0;
+  dataRecieved = ""; 
+
+  switch (modus)
+  {
+    case debug: // Debug Steuerung
+      Serial.println("debug");
+      if (stateV1 == open) 
+      { 
+        digitalWrite(V1, HIGH); 
+        Serial.println("DEBUG (Zustand): 1 open");
+      }
+      else if (stateV1 == closed) 
+      { 
+        digitalWrite(V1, LOW);
+        Serial.println("DEBUG (Zustand): 1 closed");  
+      }
+      else if (stateV2 == open) 
+      { 
+        digitalWrite(V2, HIGH);
+        Serial.println("DEBUG (Zustand): 2 open");
+      }
+      else if (stateV2 == closed) 
+      { 
+        digitalWrite(V2, LOW);
+        Serial.println("DEBUG (Zustand): 2 closed");
+      }
+      else if (stateV3 == open) 
+      { 
+        digitalWrite(V3, HIGH);
+        Serial.println("DEBUG (Zustand): 3 open");
+      }
+      else if (stateV3 == closed) 
+      { 
+        digitalWrite(V3, LOW);
+        Serial.println("DEBUG (Zustand): 3 closed");   
+      } 
+      else if (stateV4 == open) 
+      { 
+        digitalWrite(V4, HIGH);
+        Serial.println("DEBUG (Zustand): 4 open");
+      }
+      else if (stateV4 == closed)
+      { 
+        digitalWrite(V4, LOW);
+        Serial.println("DEBUG (Zustand): 4 closed");
+      } 
+    break;
+
+    case manual: // Manuelle Steuerung
+      Serial.println("manual");
+     
+    break;
+
+    case automatic: // Automatische Steuerung
+      Serial.println("auto");
+    
+    break;
+
+    default:
+    break;
   }
 }
